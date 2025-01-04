f5594fc721f623859e2ab0b750f874d592967d26
Dheeraj Gupta <dheerajgupta1902@gmail.com>
2025-01-04 22:25:25 +0530

    project21 added

---
+++ patterns.py
@@ -0,0 +1,21 @@
+# Tree pattern
+def tree(height):
+    height = height - 1
+    length = height * 2 - 1
+    star = 1
+    for i in range(1, height + 1):
+        print(("*" * star).center(length))
+        star += 2
+    print("*".center(length), end="")
+
+while True:
+    n = input("Enter a height of tree: ")
+    try:
+        n = int(n)
+        break
+    except:
+        print("Please enter a valid number!")
+
+# Drawing tree
+print("Here is your tree:\n ")
+tree(n)
